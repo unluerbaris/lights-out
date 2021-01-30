778cbad41d53dc597ee4cac4a326925bc313dc96
Baris Unluer <unluerbaris@gmail.com>
2021-01-30 23:28:16 +0900

    object interaction key info

--- Assets/Scripts/Environment/ObjectDetection.cs
+++ Assets/Scripts/Environment/ObjectDetection.cs
@@ -4,9 +4,32 @@ namespace LightsOut.Environment
 {
     public class ObjectDetection : MonoBehaviour
     {
+        [SerializeField] GameObject objectInfoCanvas;
+        bool isTextExist = false;
+
         private void OnTriggerEnter2D(Collider2D collision)
         {
+            if (objectInfoCanvas == null) return;
+            if (isTextExist) return;
+
             Debug.Log($"Thats a {collision.gameObject.name}");
+            isTextExist = true;
+            GameObject infoCanvasInstance = Instantiate(objectInfoCanvas, collision.transform.position, Quaternion.identity) as GameObject;
+            infoCanvasInstance.gameObject.transform.parent = collision.transform;
+        }
+
+        private void OnTriggerStay2D(Collider2D collision)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Debug.Log("Show SOME TEXT");
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D collision)
+        {
+            Destroy(collision.gameObject.transform.GetChild(collision.transform.childCount - 1).gameObject);
+            isTextExist = false;
         }
     }
 }
