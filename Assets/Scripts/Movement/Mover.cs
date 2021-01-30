af052b8e3a25c7c1f7f2c9a29681f38520d8a4a8
Baris Unluer <unluerbaris@gmail.com>
2021-01-30 21:56:32 +0900

    fixed player animation bug

--- Assets/Scripts/Movement/Mover.cs
+++ Assets/Scripts/Movement/Mover.cs
@@ -8,6 +8,7 @@ namespace LightsOut.Movement
 
         Vector2 normalizedVelocity;
         bool playerHasHorizontalSpeed;
+        bool playerHasVerticalSpeed;
 
         Rigidbody2D rb2D;
         Animator myAnimator;
@@ -22,13 +23,15 @@ namespace LightsOut.Movement
         {
             normalizedVelocity = new Vector2(controlThrowX, controlThrowY).normalized;
             rb2D.velocity = normalizedVelocity * speed;
-            myAnimator.SetBool("isMoving", playerHasHorizontalSpeed);
+            myAnimator.SetBool("isMoving", playerHasHorizontalSpeed || playerHasVerticalSpeed);
             FlipCharacter();
         }
 
         private void FlipCharacter()
         {
-            playerHasHorizontalSpeed = Mathf.Abs(rb2D.velocity.x) > Mathf.Epsilon;
+            playerHasHorizontalSpeed = Mathf.Abs(rb2D.velocity.x) > 0.1;
+            playerHasVerticalSpeed = Mathf.Abs(rb2D.velocity.y) > 0.1;
+
             if (playerHasHorizontalSpeed)
             {
                 transform.localScale = new Vector2(-Mathf.Sign(rb2D.velocity.x), 1f);
