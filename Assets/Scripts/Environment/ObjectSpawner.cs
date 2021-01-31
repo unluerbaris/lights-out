136772d3ab091ccb6895502859e6316a0b55c9a4
Baris Unluer <unluerbaris@gmail.com>
2021-01-31 13:43:34 +0900

    object collection ui

--- Assets/Scripts/Environment/ObjectSpawner.cs
+++ Assets/Scripts/Environment/ObjectSpawner.cs
@@ -1,20 +1,26 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace LightsOut.Environment
 {
     public class ObjectSpawner : MonoBehaviour
     {
         [SerializeField] GameObject[] objects;
+        [SerializeField] Text numOfItemsCollected;
+        [SerializeField] Text totalNumOfItems;
 
         int objectIndex = 0;
 
         void Awake()
         {
             SpawnObject();
+            totalNumOfItems.text = objects.Length.ToString();
         }
 
         public void SpawnObject()
         {
+            numOfItemsCollected.text = objectIndex.ToString();
+
             if (objectIndex >= objects.Length) return;
 
             int randomIndex = Random.Range(0, transform.childCount - 1);
